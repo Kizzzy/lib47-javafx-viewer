bc5f2a5eb58b7cd1af9f60307c993c621299e935
Kizzzy <xuhua0174@163.com>
2024-04-06 21:35:07 +0800

    [javafx-viewer] + add common file and folder operation in ViewerExecutor

--- src/main/java/cn/kizzzy/javafx/viewer/executor/AbstractViewerExecutor.java
+++ src/main/java/cn/kizzzy/javafx/viewer/executor/AbstractViewerExecutor.java
@@ -8,6 +8,8 @@ import cn.kizzzy.javafx.viewer.ViewerExecutor;
 import cn.kizzzy.javafx.viewer.ViewerExecutorArgs;
 import cn.kizzzy.vfs.tree.Leaf;
 import cn.kizzzy.vfs.tree.Node;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +17,8 @@ import org.slf4j.LoggerFactory;
 import java.awt.*;
 import java.awt.datatransfer.StringSelection;
 import java.io.File;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public abstract class AbstractViewerExecutor implements ViewerExecutor {
     
@@ -60,5 +64,65 @@ public abstract class AbstractViewerExecutor implements ViewerExecutor {
             StringSelection selection = new StringSelection(path);
             Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection, selection);
         }
+    }
+    
+    protected File showOpenFolder(ViewerExecutorArgs args, String title, Supplier<String> getter, Consumer<String> setter) {
+        Stage stage = args.getStage();
+        
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle(title);
+        if (StringHelper.isNotNullAndEmpty(getter.get())) {
+            File folder = new File(getter.get());
+            if (folder.exists()) {
+                chooser.setInitialDirectory(folder);
+            }
+        }
+        
+        File file = chooser.showDialog(stage);
+        if (file != null) {
+            setter.accept(file.getAbsolutePath());
+        }
+        
+        return file;
+    }
+    
+    protected File showOpenFile(ViewerExecutorArgs args, String title, Supplier<String> getter, Consumer<String> setter) {
+        Stage stage = args.getStage();
+        
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle(title);
+        if (StringHelper.isNotNullAndEmpty(getter.get())) {
+            File lastFolder = new File(getter.get());
+            if (lastFolder.exists()) {
+                chooser.setInitialDirectory(lastFolder);
+            }
+        }
+        
+        File file = chooser.showOpenDialog(stage);
+        if (file != null) {
+            setter.accept(file.getParent());
+        }
+        
+        return file;
+    }
+    
+    protected File showSaveFile(ViewerExecutorArgs args, String title, Supplier<String> getter, Consumer<String> setter) {
+        Stage stage = args.getStage();
+        
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle(title);
+        if (StringHelper.isNotNullAndEmpty(getter.get())) {
+            File lastFolder = new File(getter.get());
+            if (lastFolder.exists()) {
+                chooser.setInitialDirectory(lastFolder);
+            }
+        }
+        
+        File file = chooser.showSaveDialog(stage);
+        if (file != null) {
+            setter.accept(file.getParent());
+        }
+        
+        return file;
     }
 }
